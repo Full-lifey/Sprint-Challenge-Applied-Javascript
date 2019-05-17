34aacd83e3183111dc7973bae8845346c82540d1
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-17 10:44:09 -0600

    carousel working. Ready to add animations

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,35 @@
 class Carousel {
+    constructor(item){
+        this.item = item
+        this.leftButton = document.querySelector('.left-button');
+        this.rightButton = document.querySelector('.right-button');
+        this.images = document.querySelectorAll('.carousel img')
+        this.currentIndex = 0
+        this.leftButton.addEventListener('click', () => this.scrollLeft())
+        this.rightButton.addEventListener('click', () => this.scrollRight())
+        this.images[this.currentIndex].style.display = 'block'
+        
+    }
+    scrollLeft(){
+        this.images[this.currentIndex].style.display = 'none'
+        if (this.currentIndex === 0){this.currentIndex = 3}
+        else {
+            this.currentIndex -= 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
 
+    scrollRight(){
+        this.images[this.currentIndex].style.display = 'none'
+        if (this.images.length-1 === this.currentIndex){this.currentIndex = 0}
+        else {
+            this.currentIndex += 1
+        }
+        this.images[this.currentIndex].style.display = 'block'
+    }
 }
 
-let carousel = document.querySelector();
+const carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,3 +39,6 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+// carouselImages.forEach(image => new Carousel(image))
+const carouselInstance = new Carousel(carousel)
